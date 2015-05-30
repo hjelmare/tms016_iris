8f7b8b9f99b48e4bfcd77f82c67e3959400362c6
karlssonalbinka <karlsson.albin.ka@gmail.com>
2015-05-30 14:01:36 +0200

    fixat pupil & validator

--- pupil2.m
+++ pupil2.m
@@ -2,89 +2,92 @@ close all;
 clear;
 clc;
 
-%Fetching image and processing
-fileName = 'S1249R02';
-B = importdata([fileName '.jpg']);
-A = B;
-A = im2double(A);
+iris_path = 'goodEyes/';
+files = dir(iris_path);
+[nbrOfFiles, ~] = size(files);
 
-%Plotting image
-figure
-imshow(B);
-hold on
-
-%Circle parameters
-rmax = 70;
-rmin = 30;
-[x, y] = GetPupilLocation(A);
-
-%Finding pupil
-centermax = cedgefinder(x,y,rmin,rmax,A);
-xCenter_p = centermax(1);
-yCenter_p = centermax(2);
-r_p = centermax(3);
-plotcircle(xCenter_p, yCenter_p, r_p);
-
-%Finding Iris
-[xCenter_s, yCenter_s, r_s] = iris(A, xCenter_p, yCenter_p, r_p);
-plotcircle(xCenter_s, yCenter_s, r_s)
-
-%Checking if valid (more or less concentric)
-center_distance = sqrt((xCenter_p-xCenter_s)^2 + (yCenter_p-yCenter_s)^2);
-
-if center_distance < 10
-    lid = eyelid(A,xCenter_p,yCenter_p,r_p,r_s);
-    plot(lid(:,1),'b')
-    plot(lid(:,2),'r') 
-else
-    error('BAD SPECIMEN/TEST')
+for iFile = 3:nbrOfFiles
+    close all
+    
+    fileName = [iris_path files(iFile).name];
+    B = importdata(fileName);
+    A = B;
+    A = im2double(A);
+    
+    %Plotting image
+    figure
+    imshow(B);
+    hold on
+    
+    %Circle parameters
+    rmax = 70;
+    rmin = 30;
+    [x, y] = GetPupilLocation(A);
+    
+    %Finding pupil
+    centermax = cedgefinder(x,y,rmin,rmax,A);
+    xCenter_p = centermax(1);
+    yCenter_p = centermax(2);
+    r_p = centermax(3);
+    plotcircle(xCenter_p, yCenter_p, r_p);
+    
+    %Finding Iris
+    [xCenter_s, yCenter_s, r_s] = iris(A, xCenter_p, yCenter_p, r_p);
+    plotcircle(xCenter_s, yCenter_s, r_s)
+    
+    %Checking if valid (more or less concentric)
+    center_distance = sqrt((xCenter_p-xCenter_s)^2 + (yCenter_p-yCenter_s)^2);
+    
+    if center_distance < 10
+        lid = eyelid(A,xCenter_p,yCenter_p,r_p,r_s);
+        plot(lid(:,1),'b')
+        plot(lid(:,2),'r')
+    else
+        %error('BAD SPECIMEN/TEST')
+        disp('BAD SPECIMEN/TEST')
+        continue
+    end
+    
+    
+    
+    %-----------------------unwrapping of iris-------------------------------
+    [uImage, mask] = unwrap(A, r_p, r_s, [xCenter_p, yCenter_p], [xCenter_s, yCenter_s], [100 300], lid);
+    figure(2)
+    subplot(2,1,1)
+    imshow(uImage)
+    subplot(2,1,2)
+    imshow(mask)
+    
+    alpha = [pi pi/100 pi/20 pi/5];
+    beta = alpha;
+    omega = 3./beta;
+    
+    
+    im = uImage;
+    nscale = 3;
+    minWaveLength = 3;
+    mult = 2;
+    sigmaOnf = 10;
+    
+    [E0, filtersum] = gaborconvolve(im, nscale, minWaveLength, mult, sigmaOnf);
+    
+    E = E0{1};
+    %Phase quantisation
+    H1 = real(E) > 0;
+    H2 = imag(E) > 0;
+    
+    template = [H1;H2];
+    for i = 2:nscale
+        E = E0(i);
+        H1 = real(E) > 0;
+        H2 = imag(E) > 0;
+        template = [template; H1; H2];
+    end
+    
+    figure(3)
+    imshow(template)
+    
+    fileName = [fileName(1:end-3) 'mat'];
+    save(['irisTemplates/' fileName], 'template')
+    
 end
-
-
-
-%-----------------------unwrapping of iris-------------------------------
-[uImage, mask] = unwrap(A, r_p, r_s, [xCenter_p, yCenter_p], [xCenter_s, yCenter_s], [100 300], lid);
-figure(2)
-subplot(2,1,1)
-imshow(uImage)
-subplot(2,1,2)
-imshow(mask)
-
-alpha = [pi pi/100 pi/20 pi/5];
-beta = alpha;
-omega = 3./beta;
-
-% %for i = 1:length(alpha)
-% %    for j = 1:length(beta)
-%         waveletParams = [alpha; beta; omega];
-%         position = [uImage(1,1) uImage(1,2)];
-%         [ReH, ImH] = WaveletIntegral(waveletParams, position, uImage)
-% %    end
-% %end
-
-%%
-im = uImage;
-nscale = 1;
-minWaveLength = pi/20;
-mult = 2;
-sigmaOnf = 10;
-
-[E0, filtersum] = gaborconvolve(im, nscale, minWaveLength, mult, sigmaOnf);
-
-E1 = E0{1};
-%Phase quantisation
-H1 = real(E1) > 0;
-H2 = imag(E1) > 0;
-
-[lengthIm, hightIm] = size(E1);
-template = zeros(lengthIm*2, hightIm);
-for i = 1:2:hightIm
-    template(1:lengthIm, i) = H1(:,i);
-    template(lengthIm+1:end ,i) = H2(:,i);
-end
-
-figure(3)
-imshow(template)
-
-save(['irisTemplates/' fileName], 'template')
-
--- validator.m
+++ validator.m
@@ -1,6 +1,8 @@
+clc
+clear all
 
 %Load iris code files
-files = dir('irisTemplates/');
+files = dir('irisTemplates/goodEyes/');
 files = files(3:end); %Removing first two wrong inputs
 
 %Checking nbr of files
@@ -8,30 +10,48 @@ nbrFiles = length(files);
 
 result = zeros(nbrFiles*(nbrFiles-1),2);
 
-%Comparing all Iris codes to all minus self
+%Import all data:
+tic
 for i = 1:nbrFiles
-    filenameI = sprintf('irisTemplates/%s',files(i).name);
-    fileI = importdata(filenameI);
-   for j = 1:(nbrFiles-1) 
+    %filename{i} = importdata(sprintf('irisTemplates/goodEyes/%s',files(i).name));
+    filename{i} = load(sprintf('irisTemplates/goodEyes/%s',files(i).name));
+end
+toc
+
+
+%Comparing all Iris codes to all minus self
+tic
+for i = 1:nbrFiles
+
+%for i = 1:2
+   fileI = filename{i}.templateAndMask;
+   for j = 1:nbrFiles-1
       
       %Creating interval where i is not icluded
       jfile = [1:(i-1), (i+1):nbrFiles];
        
-      filenameJ = sprintf('irisTemplates/%s',files(jfile(j)).name);
-      fileJ = importdata(filenameJ);
+      fileJ = filename{jfile(j)}.templateAndMask;
       
-      %Checking match 
+      %Checking match  
       matchRatio = Matching(fileI, fileJ);
-      
+     
       %Checking if same (class = 2 for a match)
-      class = (filenameI(1:6) == filenameJ(1:6)) + 1;
+      class = min((files(i).name(1:6) == files(j).name(1:6)) + 1);
+
       
       %Storing result
       resNum = (i-1)*(nbrFiles-1) + j;
       result(resNum,class) = matchRatio;      
    end    
 end
+toc
+
 
 same = result((result(:,2) > 0),2);
 notsame = result((result(:,1) > 0),1);
-
+subplot(2,1,1)
+hist(same)
+title('same')
+subplot(2,1,2)
+hist(notsame)
+tisle('not same')
